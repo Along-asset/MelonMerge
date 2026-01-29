01ebf180b4186efc6aa52819544a9ccf707b2264
Along-asset <ljy18851733936@163.com>
2026-01-29 16:37:27 +0800

    Perf: Add vite-plugin-compression to reduce bundle size

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression()
+  ],
   resolve: {
     alias: {
       '@': '/src'
